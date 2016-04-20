341c340d1c86b879b8f4f63151549c75907d5f42
Adeyin Ikpah <adeyinamon@yahoo.com>
2016-04-20 00:19:02 -0500

    changed

--- credentials.py
+++ credentials.py
@@ -13,6 +13,6 @@ def get_nova_creds():
     d = {}
     d['username'] = os.environ['OS_USERNAME']
     d['api_key'] = os.environ['OS_PASSWORD']
-    d['auth_url'] = os.environ['OS_AUTH_URL']
+    d['auth_url'] = OS_AUTH_URL= 'http://10.245.125.5:5000/v2.0/'
     d['project_id'] = os.environ['OS_TENANT_NAME']
     return d
