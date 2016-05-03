9f98e305d1c4b4cfdd770137b9ec96fc55e44a81
adeyin <adeyinamon@yahoo.com>
2016-05-03 13:50:58 -0500

    added localhost to ip stuff

--- ceilometer.py
+++ ceilometer.py
@@ -3,7 +3,7 @@ from credentials import get_keystone_creds
 from ceilometerclient.v2 import client as ceilo
 def getCeilometer():
 
-	CEILOMETER_URL='http://10.245.125.5:8777'
+	CEILOMETER_URL='http://localhost:8777'
 	creds = get_keystone_creds()
 	ceilometer = ceilo.Client(endpoint=CEILOMETER_URL, **creds )
 	return ceilometer
