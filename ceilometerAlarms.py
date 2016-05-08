b2f53ec158b63ce798ee0595fedd05b3eb2e5d9d
adeyin centos <adeyinamon@gmail.com>
2016-05-08 13:37:28 -0400

    maded changes

--- ceilometerAlarms.py
+++ ceilometerAlarms.py
@@ -9,11 +9,11 @@ def createAlarmID():
 	    NovaID.append(server.id) #Gets the server ID number
 	if getCeilometer().alarms.list() == []:
 		for server in NovaID:
-	       		alarmdictCPU = {'name':'cpu high%s'%server, 'description':'cpu theshold', 'meter_name':'cpu_util', 'threshold':'30', 
+	       		alarmdictCPU = {'name':'1%s'%server, 'description':'cpu theshold', 'meter_name':'cpu_util', 'threshold':'30', 
 	       			'comparison_operator':'gt', 'period':'15', 'evaluation_periods':'1', 'alarm_action': 'log://',  
 	       				'q':'resource_id =%s'%server}
 
-	       		alarmdictIncomingBytes = {'name':'incoming bytes%s'%server, 'description':'node incoming bytes', 
+	       		alarmdictIncomingBytes = {'name':'2%s'%server, 'description':'node incoming bytes', 
 	       			'meter_name':'network.incoming.bytes', 'threshold':'180', 'comparison_operator':'gt', 'period':'15', 
 	       				'evaluation_periods':'1', 'alarm_action': 'log://',  'q':'resource_id =%s'%server}
 
