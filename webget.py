39d1c02209ad530e06a0ac497b466d86cb8ed190
adeyin <amonkee>
2016-05-11 10:14:47 -0400

    added random wait time to wget

--- webget.py
+++ webget.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python
 import os
 import time
+from random import randint
 def webGet(n,ip):
 	for x in xrange(n):
 		os.system('wget -O - http://%s'%ip)
-		time.sleep(3)
+		time.sleep(randint(1,6))
 
